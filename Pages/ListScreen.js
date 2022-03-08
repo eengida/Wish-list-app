b7641ead3563fdc6b1c4fcb0c814026cbc248c10
jovartzy <jzuniga3@ggc.edu>
2022-03-08 18:06:13 -0500

    Adds import statement to ListScreen

--- Pages/ListScreen.js
+++ Pages/ListScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AuthStyle from "../assets/styles/AuthStyle";
 import { View, Text } from "react-native";
